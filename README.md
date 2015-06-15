c7b1a4cc72dc97d03f7b480d099f2fd9b7f1e2bf
lethanhtuan2897 <lethanhtuan2897@gmail.com>
2015-06-15 12:18:27 +0700

    Vinaget 3.1 Script & Plugin Update

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-##  VINAGET SCRIPT & PLUGIN UPDATE
-
-[For Vinaget 2.7.0] If you want to catch our updates frequently, please download **_update-first-vng2.7.zip_** and extract it to your root vinaget files
+##  VINAGET 3.1 BETA SCRIPT & PLUGIN UPDATE
 
 <b>Homepage</b>: http://vinaget.us <br/>
-<b>Download</b>: https://github.com/giaythuytinh176/vinaget-script <br/>
+<b>Download</b>: vng31.r4.zip <br/>
 
-<b>Caution</b>: This is unofficial vinaget update, please do not use our updates for newer vinaget script if our current version supported vinaget is not same.
+<b>Caution</b>: This is vinaget 3.1 beta (unofficial), please do not use our updates for newer vinaget script if our current version supported vinaget is not same.
 
-<b>Current supported</b>: 2.7.0 Final r93 & 2.6.3 Final
+<b>Supported</b>: new vinaget 3.1 beta & plugin vinaget 2.6.3 Final
 
 <b>Changelog</b>:<br>
+> [15/06][4]: New Api Zip Link: support sh.st, ouo.io, adf.ly, bc.vc ; support password fshare.vn [3.1], [2.6.3]
+
 > [07/06][3]: Add more language words ; Add option hide plugins column
 
 > [07/06][2]: Plugin Update: mega.co.nz [2.7.0] ; Be useful for cbox get link with new api
--- plugins 2.6.3/fshare_vn.php
+++ plugins 2.6.3/fshare_vn.php
@@ -33,7 +33,7 @@ if (preg_match('#^http(s)?://([a-z0-9]+\.)?fshare\.vn/#', $url)){
 						$this->save_cookies("fshare.vn",$cookie);
 					}
 					$this->cookie = $cookie;
-					$page = $this->curl($url, $cookie, "");
+					$page = new_curl($url, $url, $cookie, "");
 
 					/* MULTI ACCOUNT */
 					if (stristr($page,"ocation: http://www.fshare.vn/logout.php") || stristr($page, "Tài khoản của quý khách đang được sử dụng để tải xuống bởi một thiết bị khác")) { // Change New Account
@@ -44,36 +44,44 @@ if (preg_match('#^http(s)?://([a-z0-9]+\.)?fshare\.vn/#', $url)){
 					}
 					/* MULTI ACCOUNT */
 					
-					if(stristr($page, 'name="FilePwdForm[pwd]"')) // Link Password
-					{	
+					if (stristr($page,'title="Tập tin có mật khẩu bảo vệ"></i>')) {  // Link Password
 						if($password) {
-							$cookie1 = $this->GetCookies($page);
-							$post = array();
 							if (preg_match('/<input type="hidden" value="(.*?)" name="fs_csrf" \/>/', $page, $value)) $fs_csrf = $value[1];
-							$page = $this->curl($url, $cookie1.'; '.$this->cookie, "FilePwdForm[pwd]={$password}&fs_csrf={$fs_csrf}");
-							if (stristr($page,'class="form-control pwd_input error" title="Mật khẩu không chính xác"')) die($this->lang['reportpass']);
+							$link = ""; $k = 0;
+							while ($k < 5) {
+								$content = json_decode(new_curl("https://www.fshare.vn/download/get", $url, $cookie, "DownloadForm[pwd]={$password}&fs_csrf={$fs_csrf}&ajax=download-form"), true);
+								if (isset($content['DownloadForm_pwd'])) die($this->lang['reportpass']);
+								if (isset($content["url"])) {
+									$link = $content["url"];
+									break;
+								}
+								$k++;
+							}
 						}
 						else die($this->lang['reportpass']);
 					}
-					if (stristr($page,'<div class="error-code">') && stristr($page, "<div class='error-msg'>")) die(Tools_get::report($Original,"dead"));					
-					elseif (preg_match('/ocation: (.*)/i', $page, $redir)) $link = trim($redir[1]); // Acc was set using Direct Link
-					elseif (stristr($page, '<form id="download-form"')) { // Click button "Tai nhanh"
-						if (preg_match('/<input type="hidden" value="(.*?)" name="fs_csrf"/', $page, $match)) $fs_csrf = $match[1];
-						$link = ""; $k = 0;
-						while ($k < 5) {
-							$content = json_decode(new_curl("https://www.fshare.vn/download/get", $url, $this->cookie, "fs_csrf={$fs_csrf}&DownloadForm%5Bpwd%5D=&ajax=download-form"), true);
-							if (isset($content["url"])) {
-								$link = $content["url"];
+					
+					if (!$link) {
+						if (stristr($page,'<div class="error-code">') && stristr($page, "<div class='error-msg'>")) die(Tools_get::report($Original,"dead"));					
+						elseif (preg_match('/ocation: (.*)/i', $page, $redir)) $link = trim($redir[1]); // Acc was set using Direct Link
+						elseif (stristr($page, '<form id="download-form"')) { // Click button "Tai nhanh"
+							if (preg_match('/<input type="hidden" value="(.*?)" name="fs_csrf"/', $page, $match)) $fs_csrf = $match[1];
+							$link = ""; $k = 0;
+							while ($k < 5) {
+								$content = json_decode(new_curl("https://www.fshare.vn/download/get", $url, $this->cookie, "fs_csrf={$fs_csrf}&DownloadForm%5Bpwd%5D=&ajax=download-form"), true);
+								if (isset($content["url"])) {
+									$link = $content["url"];
+									break;
+								}
+								$k++;
+							}
+						}
+						elseif (stristr($page,'Tải miễn phí<br>')) {
+							if($k <= $maxacc-1) {
+								$cookie = '';
+								$this->save_cookies("fshare.vn","");
 								break;
 							}
-							$k++;
-						}
-					}
-					elseif (stristr($page,'Tải miễn phí<br>')) {
-						if($k <= $maxacc-1) {
-							$cookie = '';
-							$this->save_cookies("fshare.vn","");
-							break;
 						}
 					}
 					
@@ -99,11 +107,14 @@ function new_curl($url,$url_fs,$cookie,$post) {
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_COOKIE, $cookie);
 	curl_setopt($ch, CURLOPT_HEADER, FALSE);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_REFERER, $url_fs);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	if ($post){
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post); 
+	}
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	$page = curl_exec($ch);
 	curl_close($ch);
 	return $page;
@@ -113,6 +124,6 @@ function new_curl($url,$url_fs,$cookie,$post) {
 * Vinaget Plugin: fshare.vn [Ver 3]
 * Created & Fixed: LTT
 * Year: 2015
-* Bug Fixed: LTT [06/06/2015]
+* Bug Fixed: LTT [14/06/2015]
 */
 ?>
